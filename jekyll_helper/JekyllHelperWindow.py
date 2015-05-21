ee3d3ca6017fa752540f3e1ea72fca14ae331b73
ExcaliburZero <cwellsny@nycap.rr.com>
2015-05-20 20:45:32 -0400

    Restructed main functions a bit
    Made several changes to the main functions of the program to fix some warnings given by Pylint. Also changed some global variables to object variables to make them easier to handle.
    
    Removed a few uneeded imports.
    
    Added an init function that declares the variables of the main window object and describes their purpose. Left some commented out as they are set by the finish_initialization function.

--- jekyll_helper/JekyllHelperWindow.py
+++ jekyll_helper/JekyllHelperWindow.py
@@ -28,8 +28,6 @@ import os
 import signal
 import subprocess
 from subprocess import Popen, PIPE
-from subprocess import call, PIPE
-from threading import Thread
 
 from gi.repository import Gtk # pylint: disable=E0611
 import logging
@@ -43,6 +41,30 @@ from jekyll_helper.PreferencesJekyllHelperDialog import PreferencesJekyllHelperD
 class JekyllHelperWindow(Window):
     __gtype_name__ = "JekyllHelperWindow"
 
+    # Initialize function
+    def __init__(self):
+        """Initialize the variables of the JekyllHelperWindow."""
+        # Windows
+        #self.AboutDialog = None # The window that contains information on the program
+        #self.PreferencesDialog = None # The window that contains the preference settings
+
+        # GUI elements
+        #self.jekyllVersionLabel  # The label that gives the Jekyll version number
+        #self.directoryChooser = None # The dropdown menu that allows the user to select the website directory
+        #self.serveSwitch = None # The switch that allows the user to start and end the serving of the website
+        #self.buildButton = None # The button that allows the user to build the website
+        #self.pushButton = None  # The button that allows the user to push the website
+
+        # Terminals
+        self.jekyll_serve = None # Terminal to handle website serving
+        self.jekyll_build = None # Terminal to run build command
+        self.jekyll_push = None # Terminal to run push command
+
+        # General variables
+        self.site_directory = "" # The path of the website directory
+        self.is_serving = False # If the site is currently being served or not
+
+    # Initialization function
     def finish_initializing(self, builder): # pylint: disable=E1002
         """Set up the main window"""
         super(JekyllHelperWindow, self).finish_initializing(builder)
@@ -61,7 +83,7 @@ class JekyllHelperWindow(Window):
             args = [ "jekyll -v" ]
             get_version = Popen(args, shell=True, stdin=PIPE, stdout=subprocess.PIPE)
             version = get_version.communicate()
-            return version[0];
+            return version[0]
 
         ####
         # Text labels
@@ -78,11 +100,13 @@ class JekyllHelperWindow(Window):
         self.directoryChooser = self.builder.get_object("directoryChooser")
         self.serveSwitch = self.builder.get_object("serveSwitch")
         self.buildButton = self.builder.get_object("buildButton")
+        self.pushButton = self.builder.get_object("pushButton")
 
+    ####
     # General functions
+    ####
 
     # Pull in program settings
-    global settings
     settings = Gio.Settings("net.launchpad.jekyll-helper")
 
     # Function to check if the set site directory exists
@@ -90,141 +114,120 @@ class JekyllHelperWindow(Window):
         """Check if the site directory that the user has chosen or not yet chosen exists."""
         # If no site directory has been entered
         if ( site_directory == "" ):
-            return 1;
+            return 1
         # If the chosen site directory does not exist
         elif ( os.path.exists(site_directory or "") == False ):
-            return 2;
+            return 2
         # If the chosen site directory does exist
         else:
-            return 0;
+            return 0
 
     # Set directory that stores the Jekyll website
-    global site_directory
-    site_directory = ""
-
     def set_website_directory(self, user_data):
         """Set the website's directory location."""
         print("Set Directory")
-        global site_directory
-        site_directory = self.directoryChooser.get_current_folder()
-        print(site_directory)
+        self.site_directory = self.directoryChooser.get_current_folder()
+        print(self.site_directory)
         # Check if the selected directory exists
-        if (self.site_directory_exists(site_directory) == 1):
+        if (self.site_directory_exists(self.site_directory) == 1):
             print("No site directory has been entered.")
-            return 1;
-        elif (self.site_directory_exists(site_directory) == 2):
+            return 1
+        elif (self.site_directory_exists(self.site_directory) == 2):
             print("The entered directory does not exist.")
-            return 2;
+            return 2
         else:
-            return 0;
+            return 0
 
     # Jekyll serve functions
-    global is_serving
-    is_serving = False
-
-    global jekyll_serve
-
-    def jekyll_serve_on(self, site_directory):
+    def website_serve_on(self, site_directory):
         """Begin serving website through Jekyll."""
         print("Jekyll Serve On: " + site_directory)
 
         # Get serve command from settings
-        global settings
-        print("Jekyll Serve Command: " + settings.get_string("serve-command"))
-        args = [ settings.get_string("serve-command") ]
+        print("Jekyll Serve Command: " + self.settings.get_string("serve-command"))
+        args = [ self.settings.get_string("serve-command") ]
         print(args)
 
         # Serve website
-        global jekyll_serve
-        jekyll_serve = Popen(args, cwd=site_directory, shell=True, stdin=PIPE, stdout=PIPE, preexec_fn=os.setsid)
-        global is_serving
-        is_serving = True
-        return;
+        self.jekyll_serve = Popen(args, cwd=site_directory, shell=True, stdin=PIPE, stdout=PIPE, preexec_fn=os.setsid)
+        self.is_serving = True
+        return
 
-    def jekyll_serve_off(self, site_directory):
+    def website_serve_off(self, site_directory):
         """End serving website through Jekyll."""
-        global jekyll_serve
         print("Jekyll Serve Off: " + site_directory)
         # End the serve command
-        os.killpg(jekyll_serve.pid, signal.SIGTERM)
-        global is_serving
-        is_serving = False
-        return;
+        os.killpg(self.jekyll_serve.pid, signal.SIGTERM)
+        self.is_serving = False
+        return
 
     def on_serveSwitch_stateset(self, widget, state):
         """Begin or end serving website through Jekyll based on the serveSwitch value."""
         print("Serve: " + str(state))
-        global site_directory
 
         # Run the serve fuction if the site directory exists
-        if (self.site_directory_exists(site_directory) == 0):
+        if (self.site_directory_exists(self.site_directory) == 0):
             if (state == True):
-                self.jekyll_serve_on(site_directory);
+                self.website_serve_on(self.site_directory)
             elif (state == False):
-                self.jekyll_serve_off(site_directory);
+                self.website_serve_off(self.site_directory)
             else:
                 print("Error triggering Jekyll Serve")
-            print("Is Serving: " + str(is_serving))
-        elif (self.site_directory_exists(site_directory) == 1):
+            print("Is Serving: " + str(self.is_serving))
+        elif (self.site_directory_exists(self.site_directory) == 1):
             print("No site directory has been entered.")
-        elif (self.site_directory_exists(site_directory) == 2):
+        elif (self.site_directory_exists(self.site_directory) == 2):
             print("The entered directory does not exist.")
-        return;
+        return
 
     # Jekyll build functions
-    def jekyll_build(self, site_directory):
+    def website_build(self, site_directory):
         """Builds the website through Jekyll."""
         # Get build command from settings
-        global settings
-        print("Jekyll Build Command: " + settings.get_string("build-command"))
-        args = [ settings.get_string("build-command") ]
+        print("Jekyll Build Command: " + self.settings.get_string("build-command"))
+        args = [ self.settings.get_string("build-command") ]
         print(args)
 
         # Build website
-        global jekyll_build
-        jekyll_build = Popen(args, cwd=site_directory, shell=True, stdin=PIPE, stdout=PIPE, preexec_fn=os.setsid).wait()
-        return;
+        self.jekyll_build = Popen(args, cwd=site_directory, shell=True, stdin=PIPE, stdout=PIPE, preexec_fn=os.setsid).wait()
+        return
 
     def on_buildButton_clicked(self, widget):
         """Build the website when the build button is clicked."""
-        global site_directory
-        print("Jekyll Build: " + site_directory)
+        print("Jekyll Build: " + self.site_directory)
 
         # Run the build fuction if the site directory exists
-        if (self.site_directory_exists(site_directory) == 0):
-            self.jekyll_build(site_directory)
+        if (self.site_directory_exists(self.site_directory) == 0):
+            self.website_build(self.site_directory)
             print("Sucessfully built website")
-        elif (self.site_directory_exists(site_directory) == 1):
+        elif (self.site_directory_exists(self.site_directory) == 1):
             print("No site directory has been entered.")
-        elif (self.site_directory_exists(site_directory) == 2):
+        elif (self.site_directory_exists(self.site_directory) == 2):
             print("The entered directory does not exist.")
-        return;
+        return
 
     # Push website functions
     def website_push(self, site_directory):
         """Push the website using the user set push command."""
         # Get push command from settings
-        global settings
-        print("Push Command: " + settings.get_string("push-command"))
-        args = [ settings.get_string("push-command") ]
+        print("Push Command: " + self.settings.get_string("push-command"))
+        args = [ self.settings.get_string("push-command") ]
         print(args)
 
         # Push website
-        global jekyll_push
-        jekyll_push = Popen(args, cwd=site_directory, shell=True, stdin=PIPE, stdout=PIPE, preexec_fn=os.setsid).wait()
-        return;
+        self.jekyll_push = Popen(args, cwd=site_directory, shell=True, stdin=PIPE, stdout=PIPE, preexec_fn=os.setsid).wait()
+        return
 
     def on_pushButton_clicked(self, widget):
         """Push the website when the push button is clicked."""
-        global site_directory
-        print("Push: " + site_directory)
+        print("Push: " + self.site_directory)
 
         # Run the push fuction if the site directory exists
-        if (self.site_directory_exists(site_directory) == 0):
-            self.website_push(site_directory)
+        if (self.site_directory_exists(self.site_directory) == 0):
+            self.website_push(self.site_directory)
             print("Sucessfully pushed website")
-        elif (self.site_directory_exists(site_directory) == 1):
+        elif (self.site_directory_exists(self.site_directory) == 1):
             print("No site directory has been entered.")
-        elif (self.site_directory_exists(site_directory) == 2):
+        elif (self.site_directory_exists(self.site_directory) == 2):
             print("The entered directory does not exist.")
-        return;
+        return
